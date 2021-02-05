3a60d2ffb2b2d2e87881a38f83e3ec78de1d0832
Shah <shahbhumika70@gmail.com>
2021-02-05 15:13:44 +0530

    Buy Your Cute Little Monster

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { Component } from 'react';
+import './App.css';
+import CardList from './components/cardList/cardList';
+import SearchBox from './components/searchBox/searchbox';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+  
+  render(){
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return(
+      <div className='App'>
+        <SearchBox
+        placeholder="Search the little monsters"
+        handleChange = {e => {this.setState({searchField: e.target.value})}}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
